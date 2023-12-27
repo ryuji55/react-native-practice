1e7db1d7aeebb9733492c77992540262fb4b2c4e
ryuji55 <rt.october.2@icloud.com>
2023-12-27 09:00:33 +0900

    Add:区画

--- App.tsx
+++ App.tsx
@@ -4,15 +4,35 @@ import styled from "styled-components/native";
 
 const Container = styled.View`
   flex: 1;
-  background-color: #fff;
+  background-color: #eee;
   align-items: center;
   justify-content: center;
+`;
+
+const Box = styled.View`
+  width: 100%;
+  height: 100px;
+  background-color: green;
+`;
+
+const LeftBox = styled.View`
+  width: 100px;
+  background-color: red;
+`;
+
+const RightBox = styled.View`
+  flex: 1;
+  width: 100px;
+  background-color: blue;
 `;
 
 export default function App() {
   return (
     <Container>
-      <Text>初めてのReactNativeだぜ！</Text>
+      <Box>
+        <LeftBox />
+        <RightBox />
+      </Box>
       <StatusBar style="auto" />
     </Container>
   );
